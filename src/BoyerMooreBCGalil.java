e817f01d29f0c8420fa50af360a1dbf179ddab8f
Ivan Leung <ivanjleung@gmail.com>
2022-01-25 16:33:49 -0500

    Refactor BCGalil

--- src/BoyerMooreBCGalil.java
+++ src/BoyerMooreBCGalil.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * An implementation of the Boyer Moore pattern matching algorithm which uses the Bad Character Heuristic
@@ -37,25 +37,23 @@ public class BoyerMooreBCGalil {
         }
 
         // matches will store the starting indices of each match found
-        ArrayList<Integer> matches = new ArrayList<>();
-
-        if (pattern.length() > text.length()) {
-            return matches;
-        }
+        List<Integer> matches = new ArrayList<>();
 
         int n = text.length();
         int m = pattern.length();
 
+        if (m > n) {
+            return matches;
+        }
+
         // lot is the last occurrence table built for the passed in pattern as specified by the bad character heuristic.
-        HashMap<Character, Integer> lot = (HashMap<Character, Integer>) LastOccurrenceTable.buildLastTable(pattern);
+        Map<Character, Integer> lot = LastOccurrenceTable.buildLastTable(pattern);
 
         /*
         s is the shift of the pattern with respect to the text. Or rather, it is the element in the text that we
         are comparing pattern.charAt(0) with.
          */
         int s = 0;
-        // j will keep track of which element in the pattern we are currently checking
-        int j;
 
         /*
         Instead of checking each character of the pattern (0 <- m), we will instead check (l <- m). While we do
@@ -64,20 +62,18 @@ public class BoyerMooreBCGalil {
         int l = 0;
 
         // k is the "periodicity" of the pattern
-        int k = m - FailureTable.buildFailureTable(pattern, comparator)[m - 1];
+        int period = m - FailureTable.buildFailureTable(pattern, comparator)[m - 1];
 
         /*
         We will use this "galil" flag to indicate whether we can use Zvi Galil's optimization technique upon
-        finding a match.
+        finding a match. Recall that we can use the galil rule if the period of the pattern is at least 1.
          */
-        boolean galil = false;
-        if (k >= 1) {
-            galil = true;
-        }
+        boolean useGalilRule = period >= 1;
 
         while (s <= n - m) {
             // once again recall that BM checks from right to left
-            j = m - 1;
+            // j will keep track of which element in the pattern we are currently checking
+            int j = m - 1;
 
             // so long as the characters of the pattern and text are matching keep moving left in the pattern
             while (j >= l && comparator.compare(pattern.charAt(j), text.charAt(s + j)) == 0) {
@@ -92,27 +88,25 @@ public class BoyerMooreBCGalil {
                 periodicity by shifting k elements forward and then only checking the last k to determine if there is
                 another occurrence. This shifting scheme is the one detailed in Galil's white paper.
                  */
-                if (galil) {
-                    l = m - k; // Recall we check the elements in the pattern from p[m-1] -> p[l]
-                    s += k;
+                if (useGalilRule) {
+                    l = m - period; // Recall we check the elements in the pattern from p[m - 1] -> p[l]
+                    s += period;
                 } else {
-                    s += 1;
+                    s++;
                 }
             } else {
-                // Note to self: potentially fewer operations to just always set l = 0?
-                if (l != 0) {
-                    /*
-                    If we have been exploiting Galil's rule, and we suddenly have a mismatch, it no longer suffices to
-                    only check the last k elements. To find another match we now must check them all.
-                     */
-                    l = 0;
-                }
+                /*
+                If we have been exploiting Galil's rule, and we suddenly have a mismatch, it no longer suffices to
+                only check the last k elements. To find another match we now must check them all.
+                    */
+                l = 0;
+
                 // Typical mismatch shifting scheme as detailed by the bad character heuristic.
                 int shift = lot.getOrDefault(text.charAt(s + j), -1);
                 if (shift < j) {
                     s = s + j - shift;
                 } else {
-                    s += 1;
+                    s++;
                 }
             }
         }
